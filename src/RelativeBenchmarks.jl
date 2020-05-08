b007a76989edca4700d0d8bdb203ba917bfa73d0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-05-08 02:24:21 -0400

    Update RelativeBenchmarks.jl

--- src/RelativeBenchmarks.jl
+++ src/RelativeBenchmarks.jl
@@ -7,6 +7,17 @@ import MacroTools: postwalk
 using MacroTools: MacroTools, prewalk, postwalk, @capture
 using BenchmarkTools
 
+#=
+    You set this directory with the default const or through the environment.
+    Ensure `NewImplementation.jl` and `OldImplementation.jl` are kept there.
+=#
+const DefaultBenchmarkingDir = pwd()
+const BenchmarkingDir = haskey(ENV, "BenchmarkingDir") ? ENV["BenchmarkingDir"] : DefaultBenchmarkingDir
+
+cd(BenchmarkingDir)
+isfile("NewImplmentation.jl") && isfile("OldImplmentation.jl") ||
+    error("Both `NewImplmentation.jl` and `OldImplementation.jl` must be found in the BenchmarkingDir ($BenchmarkingDir)")
+
 include("setup.jl")
 include("relspeeds.jl")
 
